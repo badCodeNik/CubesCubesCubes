c92fb3ba68a469cda4c1ba80f3e953b52fb72f94
badCodeNik <badCode.Nik@gmail.com>
2025-01-11 19:55:58 +0300

    created

---
+++ Assets/_project/Scripts/CubeInstallController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace _project.Scripts
+{
+    public class CubeInstallController : MonoBehaviour
+    {
+        private bool _isFirstCubeSet;
+    }
+}
---
+++ Assets/_project/Scripts/Installers/SceneInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace _project.Scripts.Installers
+{
+    public class SceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
